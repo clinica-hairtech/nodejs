69e1d77d0fdbe15c0e55d3b2804b8c2873a0e455
clinica-hairtech <rmeireles87@gmail.com>
2026-04-15 06:07:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,24 +3,19 @@ const app = express();
 
 app.use(express.json());
 
-// rota principal
-app.get('/', (req, res) => {
-  res.send('Servidor online 🚀');
+// TESTE NO NAVEGADOR
+app.get('/webhook', (req, res) => {
+  res.send('Webhook ativo');
 });
 
-// webhook (ESSA É A PARTE IMPORTANTE)
+// RECEBER POST
 app.post('/webhook', (req, res) => {
-  console.log('Webhook recebido:', req.body);
-
-  res.status(200).json({
-    status: 'ok'
-  });
+  console.log('Recebido:', req.body);
+  res.status(200).send('OK');
 });
 
 const PORT = process.env.PORT || 3000;
-app.get('/teste', (req, res) => {
-  res.send('Servidor funcionando OK 🚀');
-});
+
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log('Servidor rodando na porta', PORT);
 });
